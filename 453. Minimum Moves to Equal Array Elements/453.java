27df82fcb2e5c32fcb3bdd2bfcdbad882de0d4fe
Tim <47283664+qLirly@users.noreply.github.com>
2021-11-20 17:51:23 -0500

    Create 453.java

---
+++ 453. Minimum Moves to Equal Array Elements/453.java
@@ -0,0 +1,19 @@
+<code>
+class Solution {
+    public int minMoves(int[] nums) {
+        int floor = nums[0];
+        
+        long relativeSum = 0;
+        
+        for (int i : nums) {
+            if (i < floor) {
+                floor = i;
+            }
+            
+            relativeSum += i;
+        }
+        
+        return (int) (relativeSum - nums.length * floor);
+    }
+}
+</code>
